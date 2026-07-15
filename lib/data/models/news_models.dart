fb26dd3df77e1f86ae8a49564ca45d7400730f08
WeSsam-Zakaria2 <wessamzakaria@fci.bu.edu.eg>
2026-07-16 01:05:03 +0300

    Refactor API handling: Move API logic to a new file and update data model for news fetching

---
+++ lib/data/models/news_models.dart
@@ -0,0 +1,47 @@
+class NewsModel {
+  String? status;
+  int? totalResults;
+  List<Articles>? articles;
+
+  NewsModel({this.status, this.totalResults, this.articles});
+
+  NewsModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    totalResults = json['totalResults'];
+    if (json['articles'] != null) {
+      articles = <Articles>[];
+      json['articles'].forEach((v) {
+        articles?.add(Articles.fromJson(v));
+      });
+    }
+  }
+}
+
+class Articles {
+  String? author;
+  String? title;
+  String? description;
+  String? url;
+  String? urlToImage;
+
+  String? content;
+
+  Articles({
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.content,
+  });
+
+  Articles.fromJson(Map<String, dynamic> json) {
+    author = json['author'];
+    title = json['title'];
+    description = json['description'];
+    url = json['url'];
+    urlToImage = json['urlToImage'];
+
+    content = json['content'];
+  }
+}
