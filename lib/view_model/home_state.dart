33dd3fa9c21082b672b8838f00d8aeba8036a597
WeSsam-Zakaria2 <wessamzakaria@fci.bu.edu.eg>
2026-07-18 16:31:41 +0300

    Add HomeCubit and HomeState classes for state management

---
+++ lib/view_model/home_state.dart
@@ -0,0 +1,9 @@
+sealed class HomeState {}
+
+class HomeInitial extends HomeState {}
+
+class HomeLoading extends HomeState {}
+
+class HomeSuccess extends HomeState {}
+
+class HomeError extends HomeState {}
